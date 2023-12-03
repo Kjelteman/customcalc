f2485209250873345d4e009e8b663f3f017c9c80
Kjelteman <152700223+Kjelteman@users.noreply.github.com>
2023-12-03 20:13:26 +0100

    Create Selecteren.ino

---
+++ Selecteren.ino
@@ -0,0 +1,45 @@
+//SELECTIEPRINCIPE --> Hier heb ik de potentiometer zo gemaakt dat als die rechtsom gedraaid is, deze 'bevestigd'.
+
+int analogSelect = A0;
+int analogValue = A1;
+int selector = 0;
+int Term1;
+int Term2;
+int Term3;
+float RS;
+  
+float Berekening1(int Term1,int Term2,int Term3){
+  	float Antwoord1;
+    Antwoord1 = (Term1*Term1)+(Term2*Term2*Term2)- Term3;
+    return Antwoord1;
+	}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Lets have it");
+}
+
+void loop() {
+  selector = analogRead(analogSelect);
+  if(selector>750){
+    Serial.println("Nothing here");
+    delay(2000);
+  }
+  if(selector<250){
+    Serial.println("Wait up!");
+    	int selectedVal = analogRead(analogValue);
+    	int a = 2;
+        int b = 1;
+        int c = selectedVal * 0.01;
+    Serial.print("The value for Term 3 is:");
+    Serial.println(c);
+    delay(2000);
+    selector = analogRead(analogSelect);
+    if(selector>750){
+    	RS = Berekening1(a, b, c);
+      	Serial.print("The answer to your equation is:");
+     	Serial.println(RS);
+     	delay(2000);
+    }
+  }
+}   
